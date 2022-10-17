89ca8aba09e02692ae3a9541abed03ef6520a9b4
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-10-17 10:36:39 -0400

    name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
 }
 
-resource "null_resource" "cluster" {
+resource "null_resource" "localecho" {
     count = 1
     provisioner "local-exec" {
         command = "echo 'hello world' && sleep 10 && echo 'done!'"
