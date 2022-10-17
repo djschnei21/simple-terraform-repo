c6b106dbae59316f8b3606104f52f26f0a478704
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-10-17 14:01:21 -0400

    test change

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
 }
 
 resource "null_resource" "localecho" {
-    count = 2
+    count = 1
     provisioner "local-exec" {
         command = "echo 'hello world' && sleep 10 && echo 'done!'"
     }
